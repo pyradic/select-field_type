99091455492d14b4d7c5974521142cae13be090c
Ryan Thompson <service@aiwebsystems.com>
2014-11-04 01:14:54 -0600

    Doc'd

--- src/SelectFieldType.php
+++ src/SelectFieldType.php
@@ -2,14 +2,38 @@
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
+/**
+ * Class SelectFieldType
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Addon\FieldType\Select
+ */
 class SelectFieldType extends FieldTypeAddon
 {
+
+    /**
+     * Return the input HTML.
+     *
+     * @return mixed
+     */
     public function input()
     {
-        return \Form::select(
-            $this->inputName(),
-            $this->options(),
-            $this->value
-        );
+        $options = [
+            'class' => 'form-control',
+        ];
+
+        return app('form')->select($this->getFieldName(), $this->getOptions(), $this->getValue(), $options);
+    }
+
+    /**
+     * Get the selectable options.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return ['foo' => 'Foo', 'bar' => 'Bar'];
     }
 }
