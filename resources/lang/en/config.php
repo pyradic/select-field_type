837dc47b075f410ed689e8c928b6b81fe9c6b9e0
Ryan Thompson <service@aiwebsystems.com>
2015-04-08 00:58:10 -0500

    Config

--- resources/lang/en/config.php
+++ resources/lang/en/config.php
@@ -2,6 +2,8 @@
 
 return [
     'options' => [
-        'label' => 'Options'
+        'label'        => 'Available Options',
+        'instructions' => 'Enter each option like <strong>value : option</strong> on a new line. Options can be translation keys.',
+        'placeholder'  => "foo : Foo\nbar : Bar"
     ]
 ];
