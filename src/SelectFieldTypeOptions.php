fac3e8984b87d5aaa4c6e639686f8c610cfa2e20
Ryan Thompson <service@aiwebsystems.com>
2015-05-15 01:31:57 -0500

    Disable placeholder support

--- src/SelectFieldTypeOptions.php
+++ src/SelectFieldTypeOptions.php
@@ -30,6 +30,6 @@ class SelectFieldTypeOptions
             $options = $this->dispatch(new ParseOptions($options));
         }
 
-        return array_filter([null => $fieldType->getPlaceholder()] + $options);
+        return [null => $fieldType->getPlaceholder()] + $options;
     }
 }
