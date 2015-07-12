0bd99716563c0153dd6467469be420b20ca1d78b
Ryan Thompson <service@aiwebsystems.com>
2015-07-12 01:00:21 -0500

    Clean up option handling and allow overriding the handler

---
+++ src/Handler/Layouts.php
@@ -0,0 +1,53 @@
+<?php namespace Anomaly\SelectFieldType\Handler;
+
+use Anomaly\SelectFieldType\SelectFieldType;
+use Anomaly\Streams\Platform\Addon\Theme\ThemeCollection;
+use Illuminate\Config\Repository;
+use Illuminate\Filesystem\Filesystem;
+
+/**
+ * Class Layouts
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\SelectFieldType
+ */
+class Layouts
+{
+
+    /**
+     * Handle the options.
+     *
+     * @param SelectFieldType $fieldType
+     * @param ThemeCollection $themes
+     * @param Repository      $config
+     * @param Filesystem      $files
+     */
+    public function handle(SelectFieldType $fieldType, ThemeCollection $themes, Repository $config, Filesystem $files)
+    {
+        $theme = $themes->get($config->get('streams::themes.active.standard'));
+
+        $options = $files->files($theme->getPath('resources/views/layouts'));
+
+        $fieldType->setOptions(
+            array_combine(
+                array_map(
+                    function ($path) use ($theme) {
+                        return 'theme::' . ltrim(
+                            str_replace($theme->getPath('resources/views/layouts'), '', $path),
+                            '/'
+                        );
+                    },
+                    $options
+                ),
+                array_map(
+                    function ($path) use ($theme) {
+                        return ltrim(str_replace($theme->getPath('resources/views/layouts'), '', $path), '/');
+                    },
+                    $options
+                )
+            )
+        );
+    }
+}
