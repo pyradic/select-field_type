38d36e99c9a1493e15d507bd874003e6019b4e5c
Ryan Thompson <ryan@pyrocms.com>
2016-02-12 02:35:22 -0600

    Update handler for layouts to humanize the name

--- src/Handler/Layouts.php
+++ src/Handler/Layouts.php
@@ -2,6 +2,7 @@
 
 use Anomaly\SelectFieldType\SelectFieldType;
 use Anomaly\Streams\Platform\Addon\Theme\ThemeCollection;
+use Anomaly\Streams\Platform\Support\Str;
 use Illuminate\Config\Repository;
 use Illuminate\Filesystem\Filesystem;
 
@@ -23,29 +24,42 @@ class Layouts
      * @param ThemeCollection $themes
      * @param Repository      $config
      * @param Filesystem      $files
+     * @param Str             $str
      */
-    public function handle(SelectFieldType $fieldType, ThemeCollection $themes, Repository $config, Filesystem $files)
-    {
+    public function handle(
+        SelectFieldType $fieldType,
+        ThemeCollection $themes,
+        Repository $config,
+        Filesystem $files,
+        Str $str
+    ) {
         $theme = $themes->get($config->get('streams::themes.standard'));
 
         $options = $files->allFiles($theme->getPath('resources/views/layouts'));
 
-        $fieldType->setOptions(array_combine(
-            array_map(
-                function ($path) use ($theme) {
-                    return 'theme::' . ltrim(
-                        str_replace($theme->getPath('resources/views'), '', $path),
-                        '/'
-                    );
-                },
-                $options
-            ),
-            array_map(
-                function ($path) use ($theme) {
-                    return ltrim(str_replace($theme->getPath('resources/views/layouts'), '', $path), '/');
-                },
-                $options
+        $fieldType->setOptions(
+            array_combine(
+                array_map(
+                    function ($path) use ($theme) {
+                        return 'theme::' . ltrim(
+                            str_replace($theme->getPath('resources/views'), '', $path),
+                            '/'
+                        );
+                    },
+                    $options
+                ),
+                array_map(
+                    function ($path) use ($theme, $str) {
+                        return $str->humanize(
+                            basename(
+                                ltrim(str_replace($theme->getPath('resources/views/layouts'), '', $path), '/'),
+                                '.twig'
+                            )
+                        );
+                    },
+                    $options
+                )
             )
-        ));
+        );
     }
 }
