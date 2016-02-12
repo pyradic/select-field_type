b4d46da55efd716a8d178a54598c801478a7c5f6
Ryan Thompson <ryan@pyrocms.com>
2016-02-12 14:26:09 -0600

    Custom style

--- resources/js/input.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-    // Initialize form switches
-    $('.select-field_type select').each(function () {
-        $(this).select2({
-            theme: 'bootstrap'
-        });
-    });
-});
