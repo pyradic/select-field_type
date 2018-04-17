c05dbef648e28695eeadce1632f265a98b2845b9
Claas Jansen <claas.jansen@webformatik.com>
2018-04-17 12:15:41 +0200

    added german translation files

---
+++ resources/lang/de/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Select',
+    'name'        => 'Select Feldtyp',
+    'description' => 'Ein Select Feldtyp.',
+];
---
+++ resources/lang/de/config.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'mode'          => [
+        'label'        => 'Modus',
+        'instructions' => 'Welche Art Eingabefeld wollen Sie darstellen?',
+        'option'       => [
+            'search'   => 'Suche',
+            'buttons'  => 'Buttons',
+            'dropdown' => 'Dropdown',
+            'radio'    => 'Radio Buttons',
+        ],
+    ],
+    'options'       => [
+        'label'        => 'Optionen',
+        'instructions' => 'Geben Sie die Optionen unten nur im <strong>key: Value</strong> oder <strong>Value</strong> Format an. Geben Sie jede Option auf einer neuen Zeile an.',
+        'placeholder'  => 'key: Value',
+    ],
+    'default_value' => [
+        'label'        => 'Standardwert',
+        'instructions' => 'Geben Sie einen Standardwert an, falls zutreffend.',
+    ],
+];
